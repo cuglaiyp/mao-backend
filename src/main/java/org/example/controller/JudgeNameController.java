e3587cd4eed6d0630e73dea1d8ac62c552cf281d
abcuos <1585962327@qq.com>
2024-12-30 11:56:31 +0800

    校验姓名

--- src/main/java/org/example/controller/JudgeNameController.java
+++ src/main/java/org/example/controller/JudgeNameController.java
@@ -19,8 +19,14 @@ public class JudgeNameController implements IFunctionHandler<Map> {
     public Map execute(NettyHttpRequest request) {
         String player = URLDecoder.decode(request.getStringPathValue(2));
         Map res = new HashMap();
-        if (player.length() > 4) {
-            res.put("msg", "名称过长，需小于5！");
+        if (1 > player.length() || player.length() > 4) {
+            res.put("msg", "名称长度错误，需大于0小于5！");
+            res.put("code", 1);
+            return res;
+        }
+        char fc = player.charAt(0);
+        if('0' <= fc && fc <= '9') {
+            res.put("msg", "名称格式错误，首位不能为数字！");
             res.put("code", 1);
             return res;
         }
